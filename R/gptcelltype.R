0349e42025f7d9badc2d46ea4b141a124289e0b1
xiehs211 <philovo@i.smu.edu.cn>
2025-12-11 22:36:24 +0800

    增加三方api支持

--- R/gptcelltype.R
+++ R/gptcelltype.R
@@ -65,7 +65,7 @@ gptcelltype <- function(input, tissuename=NULL, model='gpt-4', topgenenumber = 1
       while (flag == 0) {
         k <- apiSXY::create_chat_completion(
           model = model,
-          message = list(list("role" = "user", "content" = paste0('Identify cell types of ',tissuename,' cells using the following markers separately for each\n row. Only provide the cell type name. Do not show numbers before the name.\n Some can be a mixture of multiple cell types.\n',paste(input[id],collapse = '\n'))))
+          messages = list(list("role" = "user", "content" = paste0('Identify cell types of ',tissuename,' cells using the following markers separately for each\n row. Only provide the cell type name. Do not show numbers before the name.\n Some can be a mixture of multiple cell types.\n',paste(input[id],collapse = '\n'))))
         )
         res <- strsplit(k$choices[,'message.content'],'\n')[[1]]
         if (length(res)==length(id))
